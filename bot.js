dcd9afb6b34022c82b75d83d768f1b2550ece630
Ortix <kyleedwardsdev@gmail.com>
2018-03-29 02:23:23 +0100

    Initial

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone:true});
+const botconfig = require("./botconfig.json");
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.log(err);
+  
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if (jsfile.length <= 0) {
+      console.log("Couldn't find commands");
+      return;
+    }
+  
+    jsfile.forEach((f, i) => {
+      let props = require(`./commands/${f}`);
+      console.log(`${f} loaded!`);
+      bot.commands.set(props.help.name, props);
+    })
+  
+});
+
+bot.on("ready", () => {
+    console.log(`${bot.user.username} is online.`);
+    bot.user.setGame("-check");
+});
+
+bot.on("message", (message) => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot, message, args);
+});
+
+bot.login(botconfig.token);
